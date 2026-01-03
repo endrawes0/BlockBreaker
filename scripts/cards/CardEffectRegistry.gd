08bba2dd721baacaa06d880b248297e3066dda20
Andrew Haddad <me@andrewwhaddad.com>
2026-01-02 19:45:01 -0500

    Load card effect scripts in registry

--- scripts/cards/CardEffectRegistry.gd
+++ scripts/cards/CardEffectRegistry.gd
@@ -1,6 +1,19 @@
 extends RefCounted
 class_name CardEffectRegistry
 
+const CardEffect = preload("res://scripts/cards/CardEffect.gd")
+const WhatDoesntKillUsCardEffect = preload("res://scripts/cards/WhatDoesntKillUsCardEffect.gd")
+const PunchCardEffect = preload("res://scripts/cards/PunchCardEffect.gd")
+const TwinCardEffect = preload("res://scripts/cards/TwinCardEffect.gd")
+const GuardCardEffect = preload("res://scripts/cards/GuardCardEffect.gd")
+const WidenCardEffect = preload("res://scripts/cards/WidenCardEffect.gd")
+const BombCardEffect = preload("res://scripts/cards/BombCardEffect.gd")
+const RallyCardEffect = preload("res://scripts/cards/RallyCardEffect.gd")
+const FocusCardEffect = preload("res://scripts/cards/FocusCardEffect.gd")
+const HasteCardEffect = preload("res://scripts/cards/HasteCardEffect.gd")
+const SlowCardEffect = preload("res://scripts/cards/SlowCardEffect.gd")
+const WoundCardEffect = preload("res://scripts/cards/WoundCardEffect.gd")
+
 var effects: Dictionary = {}
 
 func _init() -> void:
